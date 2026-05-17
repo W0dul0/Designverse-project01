104f04af1914d52e36489b0749048d2a0e8c4f7a
Kaomngo <jeffreyngov7@gmail.com>
2026-05-16 19:51:15 -0700

    Made MainPage File and cleaned App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import { AboutUs, AboutUs2} from './AboutUs.tsx'
-import { Headings, TitleIcon } from './Headings.tsx'
-import { Announcement } from './Announcement.tsx'
+import { MainPage } from './MainPage.tsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <div id="WebHeadings">
-    <TitleIcon/>
-    <div id="HeadingLinks">
-    <Headings/>
-    </div>
-    </div>
-
-    <div id="Announcement-Section"> 
-      <Announcement/>
-    </div>
-
-    <div id="AboutUs-Section">
-      <AboutUs/>
-      <AboutUs2/>
-    </div>
+    <MainPage/>
     </>
+
   )
 }
 
---
+++ src/MainPage.tsx
@@ -0,0 +1,25 @@
+import { AboutUs, AboutUs2} from './AboutUs.tsx'
+import { Headings, TitleIcon } from './Headings.tsx'
+import { Announcement } from './Announcement.tsx'
+import './MainPage.css'
+export function MainPage(){
+    return(
+    <>
+    <div id="WebHeadings">
+    <TitleIcon/>
+    <div id="HeadingLinks">
+    <Headings/>
+    </div>
+    </div>
+
+    <div id="Announcement-Section"> 
+      <Announcement/>
+    </div>
+
+    <div id="AboutUs-Section">
+      <AboutUs/>
+      <AboutUs2/>
+    </div>
+    </>
+    )
+}
