2b5f2c75cea6e76e5430e380a11ce30db79790d3
Kaomngo <jeffreyngov7@gmail.com>
2026-05-17 00:52:00 -0700

    Update MainPage.tsx

--- src/Main_Page/MainPage.tsx
+++ src/Main_Page/MainPage.tsx
@@ -1,7 +1,7 @@
 import { AboutUs } from "./MainPage_Components/AboutUs";
 import Header from "./MainPage_Components/Headings.tsx";
 import { Announcement } from "./MainPage_Components/Announcement";
-import { ContactUs } from "./MainPage_components/ContactUs";
+import { ContactUs } from "./MainPage_Components/ContactUs";
 import "./MainPage.css";
 
 export default function Main() {
