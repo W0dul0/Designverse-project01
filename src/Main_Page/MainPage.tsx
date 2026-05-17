fbfb192f7b853498bb44523f078fabda07336f78
Kaomngo <jeffreyngov7@gmail.com>
2026-05-16 22:15:19 -0700

    Test

--- src/Main_Page/MainPage.tsx
+++ src/Main_Page/MainPage.tsx
@@ -5,46 +5,9 @@ import "./MainPage.css";
 import { useState, useEffect } from "react";
 
 export default function MainPage() {
-  const [scrolled, setScrolled] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const isScrolled = window.scrollY > 150;
-      setScrolled(isScrolled);
-
-      if (!isScrolled) {
-        setMenuOpen(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <>
-      <div id="WebHeadings" className={scrolled ? "scrolled" : ""}>
-        <TitleIcon />
-
-        {(!scrolled || menuOpen) && (
-          <div id="HeadingLinks" className={menuOpen ? "fade-in" : ""}>
-            <Headings />
-          </div>
-        )}
-
-        {scrolled && !menuOpen && (
-          <div id="Hamburger" onClick={() => setMenuOpen(true)}>
-            <img src="/icons8-menu.svg" />
-          </div>
-        )}
-
-        {menuOpen && (
-          <div id="CloseMenu" onClick={() => setMenuOpen(false)}>
-            <img src="/icons8-close.svg" />
-          </div>
-        )}
-      </div>
+    <Headings/>
 
       <div id="Announcement-Section">
         <Announcement />
--- src/Main_Page/MainPage_Components/Headings.tsx
+++ src/Main_Page/MainPage_Components/Headings.tsx
@@ -1,15 +1,57 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./Headings.css";
 
-export function Headings() {
+export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 150;
+      setScrolled(isScrolled);
+
+      // Auto-close menu when scrolling back up
+      if (!isScrolled) {
+        setMenuOpen(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/shop">Shop</Link>
-      <Link to="/enterprise">Enterprise</Link>
-    </>
+    <div id="WebHeadings" className={scrolled ? "scrolled" : ""}>
+      <div id="TitleIcon">Save Bread</div>
+
+      {/* Full links when not scrolled OR menu is open */}
+      {(!scrolled || menuOpen) && (
+        <div id="HeadingLinks" className={menuOpen ? "fade-in" : ""}>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/shop">Shop</Link>
+          <Link to="/enterprise">Enterprise</Link>
+        </div>
+      )}
+
+      {/* Hamburger */}
+      {scrolled && !menuOpen && (
+        <div id="Hamburger" onClick={() => setMenuOpen(true)}>
+          <img src="/icons8-menu.svg" />
+        </div>
+      )}
+
+      {/* Close icon */}
+      {menuOpen && (
+        <div id="CloseMenu" onClick={() => setMenuOpen(false)}>
+          <img src="/icons8-close.svg" />
+        </div>
+      )}
+    </div>
   );
 }
+
 
 export function TitleIcon(){
     return(
