017aa0ff80673e4f06f00f949f409065e0cea4ed
Kaomngo <jeffreyngov7@gmail.com>
2026-05-17 05:01:53 -0700

    Test

--- src/Main_Page/MainPage_Components/Announcement.tsx
+++ src/Main_Page/MainPage_Components/Announcement.tsx
@@ -2,7 +2,7 @@ export function Announcement(){
     
     return(
         <>
-        <img src="landscape-placeholder.svg"></img>
+        <img src="landscape-placeholder.svg" id="Main_Img"></img>
         </>
     )
 }
