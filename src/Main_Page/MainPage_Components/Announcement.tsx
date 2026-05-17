c6efd457133a2b7bc3f490e60facb50a55b54b93
Kaomngo <jeffreyngov7@gmail.com>
2026-05-17 05:45:05 -0700

    Landing page

--- src/Main_Page/MainPage_Components/Announcement.tsx
+++ src/Main_Page/MainPage_Components/Announcement.tsx
@@ -1,7 +1,23 @@
 export function Announcement() {
   return (
-    <>
+    <div id="AnnouncementWrapper">
+      <div id="AnnouncementText">
+        <h2>Want to help prevent food waste?</h2>
+        <p>We bring affordable baked goods to your door!</p>
+
+        <button
+          id="ScrollButton"
+          onClick={() => {
+            document
+              .getElementById("AboutUs-Section")
+              ?.scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          Learn More
+        </button>
+      </div>
+
       <img src="/8Mvj.gif" id="Main_Img" />
-    </>
-  )
+    </div>
+  );
 }
