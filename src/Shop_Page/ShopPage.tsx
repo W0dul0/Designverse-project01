3cf0701c5393fd5a8579c0b55284bb24fad7a254
Kaomngo <jeffreyngov7@gmail.com>
2026-05-17 06:05:48 -0700

    Update ShopPage.tsx

--- src/Shop_Page/ShopPage.tsx
+++ src/Shop_Page/ShopPage.tsx
@@ -1,6 +1,7 @@
 import Header from "../Components/Headings";
 import { ContactUs } from "../Components/ContactUs";
 import LeafletMapWithLocations from "./ShopPage_Components/LeafletMapWithLocations";
+import { MiniStore } from "./ShopPage_Components/MiniStore"
 
 export default function Shop(){
     return(
